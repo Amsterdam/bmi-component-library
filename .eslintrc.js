5075b9af9d8266eb83d7e4ef874ba1d7053325dd
Fons Hettema <fonshettema93@gmail.com>
2021-05-03 16:18:03 +0200

    feature: initial setup eslint / husky / lint-staged / stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	env: {
+		es6: true,
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/eslint-recommended'],
+	parserOptions: {
+		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+		ecmaFeatures: {
+			jsx: true, // Allows for the parsing of JSX
+		},
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/ban-ts-ignore': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			2,
+			{
+				args: 'none',
+			},
+		],
+		'react/prop-types': 0,
+	},
+	settings: {
+		react: {
+			version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+		},
+	},
+};
