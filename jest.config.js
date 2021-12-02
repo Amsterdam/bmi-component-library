eb433f62e90a0f752c7ba3ab494e806188fe5c23
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-12-02 13:26:18 +0100

    chore(coverage): enabled text converage reporter for jest

--- jest.config.js
+++ jest.config.js
@@ -33,7 +33,7 @@ module.exports = {
 	// A list of reporter names that Jest uses when writing coverage reports
 	coverageReporters: [
 		// "json",
-		// "text",
+		'text',
 		'lcov',
 		// "clover"
 	],
