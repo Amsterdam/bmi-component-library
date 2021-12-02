ff58edee5282a57fc1bbcbb537c9940dee81529f
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-12-02 13:35:37 +0100

    chore(documenttable): removing dead commented out code

--- src/common/src/DocumentTable/DocumentTable.stories.tsx
+++ src/common/src/DocumentTable/DocumentTable.stories.tsx
@@ -15,43 +15,6 @@ const Template: Story<React.ComponentProps<typeof DocumentTable>> = (args) => {
 export const Default = Template.bind({});
 Default.args = {
 	rows: documents,
-	// rows: [
-	// 	{
-	// 		id: 1,
-	// 		filename: 'Bouwfoto1',
-	// 		documentDescription: 'Bouwkundig onderzoek',
-	// 		documentType: 'SOK',
-	// 		year: 2021,
-	// 	},
-	// 	{
-	// 		id: 2,
-	// 		filename: 'Overzichtskaart',
-	// 		documentDescription: 'Geotechnisch onderzoek',
-	// 		documentType: 'SOK',
-	// 		year: 2021,
-	// 	},
-	// 	{
-	// 		id: 3,
-	// 		filename: 'Metingen',
-	// 		documentDescription: 'Conditiemetingen bestaande objecten',
-	// 		documentType: 'SOK',
-	// 		year: 2021,
-	// 	},
-	// 	{
-	// 		id: 4,
-	// 		filename: 'Overzichtskaart 2',
-	// 		documentDescription: 'Geotechnisch onderzoek',
-	// 		documentType: 'SOK',
-	// 		year: 2021,
-	// 	},
-	// 	{
-	// 		id: 5,
-	// 		filename: 'Foto onderkant',
-	// 		documentDescription: 'Bouwkundig onderzoek',
-	// 		documentType: 'SOK',
-	// 		year: 2021,
-	// 	},
-	// ],
 	disableFilterRow: false,
 	disableRemoval: false,
 };
