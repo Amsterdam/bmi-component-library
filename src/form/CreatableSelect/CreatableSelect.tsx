f86e98c0a398799b3c4f61f629ed2722a76cbed5
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-12-28 19:12:06 +0100

    fix(creatable): fix issues in state management around onCreateOption and onChange

--- src/form/CreatableSelect/CreatableSelect.tsx
+++ src/form/CreatableSelect/CreatableSelect.tsx
@@ -7,17 +7,23 @@ type CreatableProps = ComponentProps<typeof Creatable>;
 export type Props = {
 	label?: string;
 	createLabel?: string;
-} & CreatableProps;
+	onChange?: (value: string) => void;
+} & Omit<CreatableProps, 'onChange'>;
+
+export type Option = { label: string; value: string };
 
 const CreatableSelect: React.FC<Props> = ({
 	inputId,
 	label,
 	options: defaultOptions,
+	value,
 	createLabel = 'Voeg toe',
 	onCreateOption,
+	onChange,
 	...props
 }) => {
 	const [options, setOptions] = useState<CreatableProps['options']>(defaultOptions);
+	const [selected, setSelected] = useState<CreatableProps['value']>(value);
 
 	useEffect(() => {
 		setOptions(defaultOptions);
@@ -28,13 +34,21 @@ const CreatableSelect: React.FC<Props> = ({
 			{label && <StyledLabel htmlFor={inputId} label={label} />}
 			<Creatable
 				styles={amsStyles}
+				value={selected}
 				inputId={inputId}
 				openMenuOnFocus
 				formatCreateLabel={(value: string) => `${createLabel} "${value}"`}
 				options={options}
+				onChange={(option) => {
+					setSelected(option);
+					onChange && onChange((option as Option)?.value ?? '');
+				}}
 				onCreateOption={(inputValue) => {
-					setOptions((prevState) => [{ value: inputValue, label: inputValue }, ...(prevState ?? [])]);
+					const newOption = { value: inputValue, label: inputValue };
+					setOptions((prevState) => [newOption, ...(prevState ?? [])]);
+					setSelected(newOption);
 					onCreateOption && onCreateOption(inputValue);
+					onChange && onChange(inputValue);
 				}}
 				{...props}
 			/>
