3be4f860644f363af1847df076312cd7ee2c271c
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-12-28 16:13:13 +0100

    test(fileupload): removed redundant argument in mock function

--- src/form/FileUpload/hooks.test.ts
+++ src/form/FileUpload/hooks.test.ts
@@ -3,7 +3,7 @@ import { act, waitFor } from '@testing-library/react';
 import { useFileUpload } from './hooks';
 import { CustomFileOrRejection } from './hooks';
 
-const getPostUrlMock = jest.fn().mockImplementation((file) => {
+const getPostUrlMock = jest.fn().mockImplementation(() => {
 	Promise.resolve(true);
 });
 const getHeadersMock = jest.fn().mockImplementation(() => Promise.resolve({}));
