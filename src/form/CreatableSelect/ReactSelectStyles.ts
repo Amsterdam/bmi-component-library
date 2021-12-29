aa68b3c9aff0f7c6f0fbbeb1becc6b39bbf7bb54
Fortunato Geelhoed <f.geelhoed@gmail.com>
2021-12-29 15:13:41 +0100

    feat(creatable): Added error state to CreatableSelect

--- src/form/CreatableSelect/ReactSelectStyles.ts
+++ src/form/CreatableSelect/ReactSelectStyles.ts
@@ -10,53 +10,56 @@ export const StyledLabel = styled(Label)<ComponentProps<typeof Label>>`
 // @see https://designsystem.amsterdam.nl/7awj1hc9f/p/6561a5-selection-dropdown
 // @see https://react-select.com/styles
 // @see https://github.com/Amsterdam/amsterdam-styled-components/blob/main/packages/asc-ui/src/components/Select/SelectStyle.ts
-const reactSelectStyles: StylesConfig = {
-	control: (styles, { isFocused }) => ({
-		...styles,
-		borderColor: `${isFocused ? '#000000' : '#767676'}`,
-		borderRadius: 0,
-		boxShadow: `${isFocused ? '0 0 0 1px #000000' : '0 0 0 0 #000000'}`,
-		'&:hover': {
+const getReactSelectStyles = (error: boolean = false) =>
+	({
+		control: (styles, { isFocused }) => ({
+			...styles,
+			borderColor: error ? `${isFocused ? '#000000' : '#EC0000'}` : `${isFocused ? '#000000' : '#767676'}`,
+			borderRadius: 0,
 			boxShadow: `${isFocused ? '0 0 0 1px #000000' : '0 0 0 0 #000000'}`,
-			borderColor: '#000000',
-		},
-	}),
-	valueContainer: (styles) => ({
-		...styles,
-		padding: '8px 12px',
-		fontSize: '1rem',
-	}),
-	indicatorsContainer: (styles) => ({
-		...styles,
-		svg: {
-			width: '28px',
-			height: '28px',
-		},
-		path: {
-			fill: '#000000',
-		},
-	}),
-	indicatorSeparator: () => ({ display: 'none' }),
-	option: (styles, { isFocused, isDisabled, isSelected }) => ({
-		...styles,
-		backgroundColor: isDisabled ? undefined : isSelected ? '#E6E6E6' : isFocused ? '#E6E6E6' : undefined,
-		color: '#000000',
-		':active': {
-			backgroundColor: '#E6E6E6',
-		},
-	}),
-	singleValue: (styles) => ({
-		...styles,
-		color: '#000000',
-	}),
-	placeholder: (styles) => ({
-		...styles,
-		color: '#000000',
-	}),
-	menu: (styles) => ({
-		...styles,
-		borderRadius: '0px',
-	}),
-};
+			'&:hover': {
+				boxShadow: error
+					? `${isFocused ? '0 0 0 1px #000000' : '0 0 0 0 #EC0000'}`
+					: `${isFocused ? '0 0 0 1px #000000' : '0 0 0 0 #000000'}`,
+				borderColor: '#000000',
+			},
+		}),
+		valueContainer: (styles) => ({
+			...styles,
+			padding: '8px 12px',
+			fontSize: '1rem',
+		}),
+		indicatorsContainer: (styles) => ({
+			...styles,
+			svg: {
+				width: '28px',
+				height: '28px',
+			},
+			path: {
+				fill: '#000000',
+			},
+		}),
+		indicatorSeparator: () => ({ display: 'none' }),
+		option: (styles, { isFocused, isDisabled, isSelected }) => ({
+			...styles,
+			backgroundColor: isDisabled ? undefined : isSelected ? '#E6E6E6' : isFocused ? '#E6E6E6' : undefined,
+			color: '#000000',
+			':active': {
+				backgroundColor: '#E6E6E6',
+			},
+		}),
+		singleValue: (styles) => ({
+			...styles,
+			color: '#000000',
+		}),
+		placeholder: (styles) => ({
+			...styles,
+			color: '#000000',
+		}),
+		menu: (styles) => ({
+			...styles,
+			borderRadius: '0px',
+		}),
+	} as StylesConfig);
 
-export default reactSelectStyles;
+export default getReactSelectStyles;
