8ce797af81c34ba329447036f6f16d7fdd1e20b8
René Noordegraaf <reno1979@gmail.com>
2022-09-22 09:59:48 +0200

    added AsynSelect to export

--- src/form/index.ts
+++ src/form/index.ts
@@ -11,3 +11,4 @@ export { default as FileUpload, Props as FileUploadProps } from './FileUpload/Fi
 export { default as FileList } from './FileUpload/FileList/FileList';
 export { CustomFile } from './FileUpload/hooks';
 export { default as CreatableSelect } from './CreatableSelect/CreatableSelect';
+export { default as AsyncSelect } from './AsyncSelect/AsyncSelect';
