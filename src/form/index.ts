791beaff91c716d42751e2de8f0c88c1f198a4ed
Elwin Schotanus <elwin.schotanus@amsterdam.nl>
2025-12-31 10:30:02 +0100

    (BADM-2208) Changed setup of npm publish and exposed FileUploadMP

--- src/form/index.ts
+++ src/form/index.ts
@@ -9,6 +9,8 @@
 
 export { default as FileUpload } from './FileUpload/FileUpload';
 export type { FileUploadProps as FileUploadProps } from './FileUpload/FileUpload';
+export { FileUpload as FileUploadMP } from './MultipartUpload/FileUpload';
+export type { FileUploadProps as FileUploadMPProps } from './MultipartUpload/FileUpload';
 export { default as FileList } from './FileUpload/FileList/FileList';
 export type { CustomFile } from './FileUpload/hooks';
 export { default as CreatableSelect } from './CreatableSelect/CreatableSelect';
